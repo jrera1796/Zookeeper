64372ad4bf12f259be65d056f27a31028b19f4ff
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2021-11-28 19:44:04 -0800

    More things

--- server.js
+++ server.js
@@ -43,9 +43,19 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
-// app.get('/api/animals', (req, res) => {
-//   res.json(animals);
-// });
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
+
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
 
 app.get('/api/animals', (req, res) => {
   let results = animals;
