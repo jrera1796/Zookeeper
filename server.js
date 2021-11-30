e785513e2d62a08739468c9e692afc7d3531009a
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2021-11-29 21:55:15 -0800

    Linked pages

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 //'middleware'
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -121,6 +122,18 @@ app.post('/api/animals', (req, res) => {
 });
 
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
